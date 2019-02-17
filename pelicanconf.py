adfe84d7b9047cf18d4bda56557dd03d1f4824d4
amradk <greydog.dk@ya.ru>
2019-02-17 19:33:55 +0400

    First commit

---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Dmitriy (aka. manul) Kuzmenko'
+SITENAME = 'Another one retro-techno blog'
+SITEURL = ''
+
+PATH = 'content'
+OG_LOCALE = 'ru_RU'
+LOCALE = 'ru_RU'
+
+TIMEZONE = 'Europe/Samara'
+
+DEFAULT_LANG = 'ru'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+MAIN_MENU = True
+THEME = 'Flex-2.2.0'
